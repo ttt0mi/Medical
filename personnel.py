d5d004b2d219aaf07d7a1dac2d09ca4f255a0126
ttt0mi <tonmide96@gmail.com>
2025-07-26 11:42:44 +0100

    clean up

--- personnel.py
+++ personnel.py
@@ -43,10 +43,11 @@ Contact Number: {self.numbers}"""
 
 class Patient(Personal):
 	__id_count = 0
-	def __init__(self, first_name: str, last_name: str, date_of_birth: str, reason: str):
+	def __init__(self, first_name: str, last_name: str, date_of_birth: str, reason: str, specialty_needed: str):
 		super().__init__(first_name, last_name, date_of_birth)
+		self.reason = reason
+		self.specialty_needed = specialty_needed
 		self.__id = self.__assign_id()
-		self.__reason = reason
 		self.__medical_history = MedicalHistory()
 
 	@classmethod
@@ -57,10 +58,31 @@ class Patient(Personal):
 	@property
 	def get_id(self): return self.__id
 
-	def add_medical_record(self):
+	@property
+	def reason(self): return self.__reason
+
+	@reason.setter
+	def reason(self, reason: str):
+		self.__validate_reason(reason)
+		self.__reason = reason
+
+	@property
+	def specialty_needed(self): return self.__specialty_needed
+
+	@specialty_needed.setter
+	def specialty_needed(self, specialty_needed: str):
+		self.__validate_reason(specialty_needed)
+		self.__specialty_needed = specialty_needed.title().strip()
+
+	@staticmethod
+	def __validate_reason(reason: str):
 		pass
 
-	def delete_medical_record(self):
+	@staticmethod
+	def __validate_specialty_needed(specialty_needed: str):
+		pass
+
+	def add_medical_record(self, medical_record):
 		pass
 
 	def view_medical_history(self):
